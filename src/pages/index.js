8516c3479505049e68730b4393adb9ba64cee328
Hamilton <hamdiggy@gmail.com>
2019-11-19 09:31:03 -0800

    changed cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainPage = () => {
 	return (
-		<div className="card">I am the default page</div>
+		<div className="card pad">I am the default page</div>
 	);
 }
 
--- src/pages/learn.js
+++ src/pages/learn.js
@@ -28,25 +28,23 @@ const LearnPage = () => {
 	);
 
 	return (
-		<div className="card">
-			<Switch>
-				<Route exact path={path}>
-					<LearnIntroPage path={path}/>
-				</Route>
-				<Route exact path={`${path}/1`}>
-					<Learn1Page path={path}/>
-				</Route>
-				<Route exact path={`${path}/2`}>
-					<Learn2Page path={path} fonts={fonts} setFont={setFont} />
-				</Route>
-				<Route exact path={`${path}/3`}>
-					<Learn3Page path={path} fonts={fonts} setFont={setFont} />
-				</Route>
-				<Route exact path={`${path}/4`}>
-					<Learn4Page path={path} fonts={fonts} />
-				</Route>
-			</Switch>
-		</div>
+		<Switch>
+			<Route exact path={path}>
+				<LearnIntroPage path={path}/>
+			</Route>
+			<Route exact path={`${path}/1`}>
+				<Learn1Page path={path}/>
+			</Route>
+			<Route exact path={`${path}/2`}>
+				<Learn2Page path={path} fonts={fonts} setFont={setFont} />
+			</Route>
+			<Route exact path={`${path}/3`}>
+				<Learn3Page path={path} fonts={fonts} setFont={setFont} />
+			</Route>
+			<Route exact path={`${path}/4`}>
+				<Learn4Page path={path} fonts={fonts} />
+			</Route>
+		</Switch>
 	);
 }
 
@@ -59,39 +57,49 @@ const LearnPage = () => {
 
 
 const LearnIntroPage = (props) => {
-	return (<>
-		<h3>Learn about react stuff.</h3>
-
-		<p>I should probably mention this page is only useful if you also look at the <a href="https://github.com/bronkula/react-test" target="_blank">github source repository</a>. The build will just seem silly by itself.</p>
-
-		<div className="flex-parent">
-			<div className="flex-child"/>
-			<Link className="btn" to={`${props.path}/1`}>Start!</Link>
+	return (<div class="card">
+		<div class="pad">
+			<h3>Learn about react stuff.</h3>
+	
+			<p>I should probably mention this page is only useful if you also look at the <a href="https://github.com/bronkula/react-test" target="_blank" rel="noopener noreferrer">github source repository</a>. The build will just seem silly by itself.</p>
 		</div>
-	</>);
+
+
+		<div class="pad line-up">
+			<div className="flex-parent">
+				<div className="flex-child"/>
+				<Link className="btn" to={`${props.path}/1`}>Start!</Link>
+			</div>
+		</div>
+	</div>);
 }
 
 
 const Learn1Page = ({path}) => {
 	let arr = ['Red','Blue','Green','Cyan','Magenta','Yellow'];
 	return (
-		<>
-			<h3>Lists</h3>
+		<div class="card">
+			<div class="pad">
+				<h3>Lists</h3>
+	
+				<hr/>
+				<div>Inline List</div>
+				{arr.map((o,i)=>
+					<div key={i}>{o}</div>
+				)}
+	
+				<hr/>
+				<div>Component List</div>
+				<NumberList data={arr} />
+			</div>
 
-			<hr/>
-			<div>Inline List</div>
-			{arr.map((o,i)=>
-				<div key={i}>{o}</div>
-			)}
-
-			<hr/>
-			<div>Component List</div>
-			<NumberList data={arr} />
-
-			<NextPrev path={path} />
-		</>
+			<div class="pad line-up">
+				<NextPrev path={path} />
+			</div>
+		</div>
 	);
 }
+
 
 const Learn2Page = ({path,fonts,setFont}) => {
 
@@ -112,29 +120,32 @@ const Learn2Page = ({path,fonts,setFont}) => {
 	}
 
 	return (
-		<>
-			<h3>React forms</h3>
-
-			<div>Choose a sans-serif font. Such as:
-				<ClickList data={fontlist} callback={setFont} delimiter=", " />
+		<div class="card">
+			<div class="pad">
+				<h3>React forms</h3>
+	
+				<div>Choose a sans-serif font. Such as:
+					<ClickList data={fontlist} callback={setFont} delimiter=", " />
+				</div>
+	
+				<hr/>
+				<div>This input will update all values instantly.</div>
+				<input type="text" value={fonts.first} onChange={handleInput} />
+	
+				<hr/>
+				<div>Don't try to change values <em>on submit</em> of <em>controlled inputs</em>.</div>
+				<form onSubmit={handleSubmit}>
+					<input type="text" value={fonts.first} onChange={handleChange} />
+				</form>
+	
+				<hr/>
+				<div style={{fontFamily:fonts.first}}>The chosen font is {fonts.first}.</div>
 			</div>
-
-			<hr/>
-			<div>This input will update all values instantly.</div>
-			<input type="text" value={fonts.first} onChange={handleInput} />
-
-			<hr/>
-			<div>Don't try to change values <em>on submit</em> of <em>controlled inputs</em>.</div>
-			<form onSubmit={handleSubmit}>
-				<input type="text" value={fonts.first} onChange={handleChange} />
-			</form>
-
-			<hr/>
-			<div style={{fontFamily:fonts.first}}>The chosen font is {fonts.first}.</div>
-
-			<hr/>
-			<NextPrev path={path} />
-		</>
+			
+			<div class="pad line-up">
+				<NextPrev path={path} />
+			</div>
+		</div>
 	);
 }
 
@@ -145,37 +156,43 @@ const Learn3Page = ({path,fonts,setFont}) => {
 	}
 
 	return (
-		<>
-			<h3>Select Pulldowns</h3>
-
-			<hr/>
-			<select value={fonts.second} onChange={handleChange}>
-				<option value="times new roman">Times New Roman</option>
-				<option value="cambria">Cambria</option>
-				<option value="georgia">Georgia</option>
-				<option value="serif">Serif</option>
-			</select>
-
-			<div style={{fontFamily:fonts.second}}>The chosen font is {fonts.second}.</div>
-
-			<hr/>
-			<NextPrev path={path} />
-		</>
+		<div class="card">
+			<div class="pad">
+				<h3>Select Pulldowns</h3>
+	
+				<hr/>
+				<select value={fonts.second} onChange={handleChange}>
+					<option value="times new roman">Times New Roman</option>
+					<option value="cambria">Cambria</option>
+					<option value="georgia">Georgia</option>
+					<option value="serif">Serif</option>
+				</select>
+	
+				<div style={{fontFamily:fonts.second}}>The chosen font is {fonts.second}.</div>
+			</div>
+			
+			<div class="pad line-up">
+				<NextPrev path={path} />
+			</div>
+		</div>
 	);
 }
 
 const Learn4Page = ({path,fonts}) => {
 
 	return (
-		<>
-			<h3>Now you can see your chosen fonts.</h3>
-
-			<div style={{fontFamily:fonts.first}}>The first chosen font is {fonts.first}.</div>
-			<div style={{fontFamily:fonts.second}}>The second chosen font is {fonts.second}.</div>
-
-			<hr/>
-			<Paginator prev={['Start Over',`${path}/2`]} next={['Finish','/']} />
-		</>
+		<div class="card">
+			<div class="pad">
+				<h3>Now you can see your chosen fonts.</h3>
+	
+				<div style={{fontFamily:fonts.first}}>The first chosen font is {fonts.first}.</div>
+				<div style={{fontFamily:fonts.second}}>The second chosen font is {fonts.second}.</div>
+			</div>
+			
+			<div class="pad line-up">
+				<Paginator prev={['Start Over',`${path}/2`]} next={['Finish','/']} />
+			</div>
+		</div>
 	);
 }
 
